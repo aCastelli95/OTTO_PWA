05bec668c786f6b3c452ef9f535ea165ad30c103
Alan Castelli <alan.castelli@fluxit.com.ar>
2020-01-04 15:33:56 -0300

    añadido todos los botones junto a sus referencias en el html

--- js/events/eventsButtons.js
+++ js/events/eventsButtons.js
@@ -1,40 +1,38 @@
-var b1 = document.getElementById('botonX1');
-var b2 = document.getElementById('botonX2');
-/*var b3 = document.getElementById('botonX3');
-var b4 = document.getElementById('botonX4');
-var b5 = document.getElementById('botonX5');
-var b6 = document.getElementById('botonX6');
-var b7 = document.getElementById('botonX7');
-var b8 = document.getElementById('botonX8');
-var b9 = document.getElementById('botonX9');
-var b10 = document.getElementById('botonX10');
-* */
+var b0 = document.getElementById("walkFordward");
+var b1 = document.getElementById("walkBackground");
+var b2 = document.getElementById("shakeLeg");
+var b3 = document.getElementById("moonWalker");
+var b4 = document.getElementById("crusaito");
+var b5 = document.getElementById("flapping");
+var b6 = document.getElementById("tiptoeSwing");
+var b7 = document.getElementById("jitter");
+var b8 = document.getElementById("happy");
+var b9 = document.getElementById("love");
+var b10 = document.getElementById("fart");
+var b11 = document.getElementById("end");
 
-var laFunc = function windowsAlertText(event){
+var laFunc = function windowsAlertText(event) {
   console.log(event);
-  //window.alert('Tocaste el boton' + event.path[0].id);
   sendCommand(); //send mensaje de device a otto
-
-  // aca tiene que ir cuando esta conectado ya el bluetooth, sino ventana emergente
 };
 
 window.onload = function() {
-    
-  b1.addEventListener("click", laFunc );
-  b2.addEventListener("click", laFunc );
-  /**b3.addEventListener("click", laFunc );
-  b4.addEventListener("click", laFunc );
-  b5.addEventListener("click", laFunc );
-  b6.addEventListener("click", laFunc );
-  b7.addEventListener("click", laFunc );
-  b8.addEventListener("click", laFunc );
-  b9.addEventListener("click", laFunc );
-  b10.addEventListener("click", laFunc );
- */
+  this.b0.addEventListener("click", laFunc(0));
+  this.b1.addEventListener("click", laFunc(1));
+  this.b2.addEventListener("click", laFunc(2));
+  this.b3.addEventListener("click", laFunc(3));
+  this.b4.addEventListener("click", laFunc(4));
+  this.b5.addEventListener("click", laFunc(5));
+  this.b6.addEventListener("click", laFunc(6));
+  this.b7.addEventListener("click", laFunc(7));
+  this.b8.addEventListener("click", laFunc(8));
+  this.b9.addEventListener("click", laFunc(9));
+  this.b10.addEventListener("click", laFunc(10));
+  this.b11.addEventListener("click", laFunc(11));
 };
 
-function sendCommand(){
-  data = "Esto proviene de la PWA";
+function sendCommand(value) {
+  data = "0001|03|" + value + "|200000000";
 
   if (!data || !characteristicCache) {
     return;
@@ -42,11 +40,8 @@ function sendCommand(){
 
   writeToCharacteristic(characteristicCache, data);
   console.log(data);
-
 }
 
 function writeToCharacteristic(characteristic, data) {
   characteristic.writeValue(new TextEncoder().encode(data));
 }
-
-
--- sw.js
+++ sw.js
@@ -1,66 +1,61 @@
-importScripts('js/sw-utils.js');
+importScripts("js/sw-utils.js");
 
-const STATIC_CACHE    = 'static-v1'; // EL corazon de la app esta en esta cache( template, css basicos, etc.)
-const DYNAMIC_CACHE   = 'dynamic-v1'; // Contenido como imagenes o pdf que varian constantementes por mantenimiento de aplicativos
-const INMUTABLE_CACHE = 'inmutable-v1'; // Contiene todas las urls de librerias externas que no van a cambiar, ejemplo Boostrap
+const STATIC_CACHE = "static-v2"; // EL corazon de la app esta en esta cache( template, css basicos, etc.)
+const DYNAMIC_CACHE = "dynamic-v2"; // Contenido como imagenes o pdf que varian constantementes por mantenimiento de aplicativos
+const INMUTABLE_CACHE = "inmutable-v2"; // Contiene todas las urls de librerias externas que no van a cambiar, ejemplo Boostrap
 
 const APP_SHELL = [
-    //'/',
-    'index.html',
-    'css/style.css',
-    'img/otto.ino',
-    'js/app.js',
-    'js/sw-utils.js',
-    'js/events/eventsButtons.js'
+  //'/',
+  "index.html",
+  "css/style.css",
+  "img/otto.ino",
+  "js/app.js",
+  "js/sw-utils.js",
+  "js/events/eventsButtons.js"
 ];
 
 const APP_SHELL_INMUTABLE = [
-    'https://use.fontawesome.com/releases/v5.11.2/css/all.css',
-    'css/bootstrap.min.css',
-    'css/mdb.min.css',
-    'js/jquery.min.js',
-    'js/popper.min.js',
-    'js/bootstrap.min.js',
-    'js/mdb.min.js',
+  "https://use.fontawesome.com/releases/v5.11.2/css/all.css",
+  "css/bootstrap.min.css",
+  "css/mdb.min.css",
+  "js/jquery.min.js",
+  "js/popper.min.js",
+  "js/bootstrap.min.js",
+  "js/mdb.min.js"
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener("install", e => {
+  const cacheStatic = caches
+    .open(STATIC_CACHE)
+    .then(cache => cache.addAll(APP_SHELL));
 
-    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
-        cache.addAll( APP_SHELL ));
+  const cacheInmutable = caches
+    .open(INMUTABLE_CACHE)
+    .then(cache => cache.addAll(APP_SHELL_INMUTABLE))
+    .catch(e => console.log(e));
 
-    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
-        cache.addAll( APP_SHELL_INMUTABLE )).catch(e => console.log(e));
-
-    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
-
+  e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 });
-
 
 /**Contiene una activacion de los recorridos actualizar. Si encontramos una cache
  * con el titulo de mi version que se deploya en PROD o en la composicion se encuentra
- * que contiene parte de su nombre, entonces la borra. 
+ * que contiene parte de su nombre, entonces la borra.
  * Esto siempre mantiene actualizada solo la estatica
  */
-self.addEventListener('activate', e => {
+self.addEventListener("activate", e => {
+  const respuesta = caches.keys().then(keys => {
+    keys.forEach(key => {
+      if (key !== STATIC_CACHE && key.includes("static")) {
+        return caches.delete(key);
+      }
 
-    const respuesta = caches.keys().then( keys => {
-
-        keys.forEach( key => {
-            if (  key !== STATIC_CACHE && key.includes('static') ) {
-                return caches.delete(key);
-            }
-
-            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
-                return caches.delete(key);
-            }
-
-        });
-
+      if (key !== DYNAMIC_CACHE && key.includes("dynamic")) {
+        return caches.delete(key);
+      }
     });
+  });
 
-    e.waitUntil( respuesta );
-
+  e.waitUntil(respuesta);
 });
 
 /**
@@ -69,21 +64,16 @@ self.addEventListener('activate', e => {
  * pueden ir variando, ya sean en tamaño, contenido, o lo que fuese. De esta forma, para no tener que traernos nuevamente
  * lo mismo, se evalua actualizar algunas partes de la cache,por eso el actualizarCacheDinamico
  */
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
+  const respuesta = caches.match(e.request).then(res => {
+    if (res) {
+      return res;
+    } else {
+      return fetch(e.request).then(newRes => {
+        return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+      });
+    }
+  });
 
-    const respuesta = caches.match( e.request ).then( res => {
-
-        if ( res ) {
-            return res;
-        } else {
-            return fetch( e.request ).then( newRes => {
-                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes )
-            });
-        }
-    });
-
-    e.respondWith( respuesta );
-
+  e.respondWith(respuesta);
 });
-
-
