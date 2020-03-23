359fce9d1b46cb16799cf6b1bd70b97dbb3a49f7
Alan Castelli <alan.castelli@fluxit.com.ar>
2020-03-23 20:14:45 -0300

    SIncronizando conectar y desconectar

--- js/events/eventsBluetooth.js
+++ js/events/eventsBluetooth.js
@@ -1,132 +1,132 @@
-var connectButton = document.getElementById('bluetooth'); //conectar_bluetooth
+var connectButton = document.getElementById('bluetooth');  // conectar_bluetooth
 var disconnectButton = document.getElementById('desconectar_bluetooth');
 var deviceCache = null;
 let characteristicCache = null;
 
-//Conect
-connectButton.addEventListener("click", function(){
-    console.log("Activaste el bluetooth!!");
-    connect();
-    // Sincronizar con arduino
+// Conect
+connectButton.addEventListener('click', e => {
+  console.log('Activaste el bluetooth!!');
+  e.waitUntil(connect());
+  // Sincronizar con arduino
 });
 
-//Desconectar de arduino
-disconnectButton.addEventListener("click", function(){
-    console.log("Desconectar el bluetooth!!");
-    disconnect();
-    //Desconectar de arduino
+// Desconectar de arduino
+disconnectButton.addEventListener('click', e => {
+  console.log('Desconectar el bluetooth!!');
+  e.waitUntil(disconnect());
+  // Desconectar de arduino
 });
 
 // Launch Bluetooth device chooser and connect to the selected
 function connect() {
+  return (deviceCache ? Promise.resolve(deviceCache) : requestBluetoothDevice())
+      .then(device => connectDeviceAndCacheCharacteristic(device))
+      .then(characteristic => startNotifications(characteristic))
+      .catch(error => console.log(error));
+}
 
-    return (deviceCache ? Promise.resolve(deviceCache) :
-      requestBluetoothDevice()).
-      then(device => connectDeviceAndCacheCharacteristic(device)).
-      then(characteristic => startNotifications(characteristic)).
-      catch(error => console.log(error));
-  }
-  
 // Disconnect from the connected device
 function disconnect() {
-    title = "Desconectando";
-    if (deviceCache) {
-        descripcion = 'Desconectando device de Cachee "' + deviceCache.name + '" bluetooth device...';
-        notificationWarning(title,texto);
-        deviceCache.removeEventListener('gattserverdisconnected',
-            handleDisconnection);
-    
-        if (deviceCache.gatt.connected) {
-          deviceCache.gatt.disconnect();
-          descripcion = '"' + deviceCache.name + '" dispositivo Bluetooth desconectado!!"';
-          notificationWarning(title,texto);
-        }
-        else {
-          description = '"' + deviceCache.name +'" El dispositivo ya se encuentra desconectado';
-          notificationWarning(title,texto);
-        }
-      }
-    
-      if (characteristicCache) {
-        characteristicCache.removeEventListener('characteristicvaluechanged',
-            handleCharacteristicValueChanged);
-        characteristicCache = null;
-      }
+  title = 'Desconectando';
+  if (deviceCache) {
+    descripcion = 'Desconectando device de Cachee "' + deviceCache.name +
+        '" bluetooth device...';
+    notificationWarning(title, descripcion);
+    deviceCache.removeEventListener(
+        'gattserverdisconnected', handleDisconnection);
 
-      deviceCache = null;
+    if (deviceCache.gatt.connected) {
+      deviceCache.gatt.disconnect();
+      descripcion =
+          '"' + deviceCache.name + '" dispositivo Bluetooth desconectado!!"';
+      notificationWarning(title, descripcion);
+    } else {
+      description = '"' + deviceCache.name +
+          '" El dispositivo ya se encuentra desconectado';
+      notificationWarning(title, descripcion);
+    }
+  }
 
+  if (characteristicCache) {
+    characteristicCache.removeEventListener(
+        'characteristicvaluechanged', handleCharacteristicValueChanged);
+    characteristicCache = null;
+  }
+
+  deviceCache = null;
 }
 
 function requestBluetoothDevice() {
-    //Busca a partir de la API -> navigator.bluetooth.requestDevice
-    // el disopsitivo con service 0xFFE0 identificacion unica
-    console.log('Requesting bluetooth device...');
+  // Busca a partir de la API -> navigator.bluetooth.requestDevice
+  // el disopsitivo con service 0xFFE0 identificacion unica
+  console.log('Requesting bluetooth device...');
 
-    return navigator.bluetooth.requestDevice({
+  return navigator.bluetooth
+      .requestDevice({
         filters: [{services: [0xFFE0]}],
-    }).
-      then(device => {
+      })
+      .then(device => {
         console.log('"' + device.name + '" bluetooth device selected');
         deviceCache = device;
-        deviceCache.addEventListener('gattserverdisconnected',
-        handleDisconnection);
+        deviceCache.addEventListener(
+            'gattserverdisconnected', handleDisconnection);
         return deviceCache;
       });
+}
+
+// Reconexion al mismo dispositivo anterior
+function handleDisconnection(event) {
+  let device = event.target;
+
+  console.log(
+      '"' + device.name +
+      '" bluetooth device disconnected, trying to reconnect...');
+
+  connectDeviceAndCacheCharacteristic(device)
+      .then(characteristic => startNotifications(characteristic))
+      .catch(error => console.log(error));
+}
+
+// Conexión al dispositivo Bluetooth
+function connectDeviceAndCacheCharacteristic(device) {
+  if (device.gatt.connected && characteristicCache) {
+    return Promise.resolve(characteristicCache);
   }
 
-  //Reconexion al mismo dispositivo anterior
-  function handleDisconnection(event) {
-    let device = event.target;
-  
-    console.log('"' + device.name +
-        '" bluetooth device disconnected, trying to reconnect...');
-  
-    connectDeviceAndCacheCharacteristic(device).
-        then(characteristic => startNotifications(characteristic)).
-        catch(error => console.log(error));
-  }
-  
-  // Conexión al dispositivo Bluetooth
-  function connectDeviceAndCacheCharacteristic(device) {
-    if (device.gatt.connected && characteristicCache) {
-        return Promise.resolve(characteristicCache);
-      }
-    
-      console.log('Connecting to GATT server...');
-    
-      return device.gatt.connect().
-          then(server => {
-            console.log('GATT server connected, getting service...');
-            return server.getPrimaryService(0xFFE0);
-          }).
-          then(service => {
-            console.log('Service found, getting characteristic...');
-            return service.getCharacteristic(0xFFE1);
-          }).
-          then(characteristic => {
-            console.log('Characteristic found');
-            characteristicCache = characteristic;
-            return characteristicCache;
-          });
-  }
-  
-  // Enable the characteristic changes notification
-  /**Funcion para realizar notificado - sincronizado los dispositivos */
-  function startNotifications(characteristic) {
-    console.log('Starting notifications...');
-    return characteristic.startNotifications().
-      then(() => {
-        title= "Bluetooth - Conectado";
-        description = "Conectado con Fabiotto";
-        notificationSucces(title,description);
-        characteristic.addEventListener('characteristicvaluechanged',
-            handleCharacteristicValueChanged);
+  console.log('Connecting to GATT server...');
+
+  return device.gatt.connect()
+      .then(server => {
+        console.log('GATT server connected, getting service...');
+        return server.getPrimaryService(0xFFE0);
+      })
+      .then(service => {
+        console.log('Service found, getting characteristic...');
+        return service.getCharacteristic(0xFFE1);
+      })
+      .then(characteristic => {
+        console.log('Characteristic found');
+        characteristicCache = characteristic;
+        return characteristicCache;
       });
-  }
+}
 
-  
+// Enable the characteristic changes notification
+/**Funcion para realizar notificado - sincronizado los dispositivos */
+function startNotifications(characteristic) {
+  console.log('Starting notifications...');
+  return characteristic.startNotifications().then(() => {
+    title = 'Bluetooth - Conectado';
+    description = 'Conectado con Fabiotto';
+    notificationSucces(title, description);
+    characteristic.addEventListener(
+        'characteristicvaluechanged', handleCharacteristicValueChanged);
+  });
+}
+
+
 // Data receiving
 function handleCharacteristicValueChanged(event) {
-    let value = new TextDecoder().decode(event.target.value);
-    console.log(value);
-  }
+  let value = new TextDecoder().decode(event.target.value);
+  console.log(value);
+}
--- sw.js
+++ sw.js
@@ -1,61 +1,57 @@
-importScripts("js/sw-utils.js");
+importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = "static-v1"; // EL corazon de la app esta en esta cache( template, css basicos, etc.)
-const DYNAMIC_CACHE = "dynamic-v1"; // Contenido como imagenes o pdf que varian constantementes por mantenimiento de aplicativos
-const INMUTABLE_CACHE = "inmutable-v1"; // Contiene todas las urls de librerias externas que no van a cambiar, ejemplo Boostrap
+const STATIC_CACHE = 'static-v1';  // EL corazon de la app esta en esta cache(
+                                   // template, css basicos, etc.)
+const DYNAMIC_CACHE =
+    'dynamic-v1';  // Contenido como imagenes o pdf que varian constantementes
+                   // por mantenimiento de aplicativos
+const INMUTABLE_CACHE =
+    'inmutable-v1';  // Contiene todas las urls de librerias externas que no van
+                     // a cambiar, ejemplo Boostrap
 
 const APP_SHELL = [
   //"/"",
-  "index.html",
-  "img/otto.ino",
-  "img/otto.png",
-  "css/style.css",
-  "js/app.js",
-  "js/style.js",
-  "js/sw-utils.js",
-  "js/toastr.js",
-  "js/events/eventsButtons.js",
-  "js/events/eventsBluetooth.js"
-  
+  'index.html', 'img/otto.ino', 'img/otto.png', 'css/style.css', 'js/app.js',
+  'js/style.js', 'js/sw-utils.js', 'js/toastr.js', 'js/events/eventsButtons.js',
+  'js/events/eventsBluetooth.js'
+
 ];
 
 const APP_SHELL_INMUTABLE = [
-  "https://use.fontawesome.com/releases/v5.11.2/css/all.css",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css",
-  "https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js",
-  "https://kit.fontawesome.com/66489ab8da.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+  'https://use.fontawesome.com/releases/v5.11.2/css/all.css',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css',
+  'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js',
+  'https://kit.fontawesome.com/66489ab8da.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css'
 ];
 
-self.addEventListener("install", e => {
-  const cacheStatic = caches
-    .open(STATIC_CACHE)
-    .then(cache => cache.addAll(APP_SHELL));
+self.addEventListener('install', e => {
+  const cacheStatic =
+      caches.open(STATIC_CACHE).then(cache => cache.addAll(APP_SHELL));
 
-  const cacheInmutable = caches
-    .open(INMUTABLE_CACHE)
-    .then(cache => cache.addAll(APP_SHELL_INMUTABLE))
-    .catch(e => console.log(e));
+  const cacheInmutable = caches.open(INMUTABLE_CACHE)
+                             .then(cache => cache.addAll(APP_SHELL_INMUTABLE))
+                             .catch(e => console.log(e));
 
   e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 });
 
-/**Contiene una activacion de los recorridos actualizar. Si encontramos una cache
- * con el titulo de mi version que se deploya en PROD o en la composicion se encuentra
- * que contiene parte de su nombre, entonces la borra.
- * Esto siempre mantiene actualizada solo la estatica
+/**Contiene una activacion de los recorridos actualizar. Si encontramos una
+ * cache con el titulo de mi version que se deploya en PROD o en la composicion
+ * se encuentra que contiene parte de su nombre, entonces la borra. Esto siempre
+ * mantiene actualizada solo la estatica
  */
-self.addEventListener("activate", e => {
+self.addEventListener('activate', e => {
   const respuesta = caches.keys().then(keys => {
     keys.forEach(key => {
-      if (key !== STATIC_CACHE && key.includes("static")) {
+      if (key !== STATIC_CACHE && key.includes('static')) {
         return caches.delete(key);
       }
 
-      if (key !== DYNAMIC_CACHE && key.includes("dynamic")) {
+      if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
         return caches.delete(key);
       }
     });
@@ -65,12 +61,14 @@ self.addEventListener("activate", e => {
 });
 
 /**
- * Como indica la palabra, va buscar los elementos que necesita, llamese templates o lo que fuera.
- * Dada las caracteristicas de la Cache dinamica, sucede aveces que algunos elementos, como imagenes o pdf
- * pueden ir variando, ya sean en tamaño, contenido, o lo que fuese. De esta forma, para no tener que traernos nuevamente
- * lo mismo, se evalua actualizar algunas partes de la cache,por eso el actualizarCacheDinamico
+ * Como indica la palabra, va buscar los elementos que necesita, llamese
+ * templates o lo que fuera. Dada las caracteristicas de la Cache dinamica,
+ * sucede aveces que algunos elementos, como imagenes o pdf pueden ir variando,
+ * ya sean en tamaño, contenido, o lo que fuese. De esta forma, para no tener
+ * que traernos nuevamente lo mismo, se evalua actualizar algunas partes de la
+ * cache,por eso el actualizarCacheDinamico
  */
-self.addEventListener("fetch", e => {
+self.addEventListener('fetch', e => {
   const respuesta = caches.match(e.request).then(res => {
     if (res) {
       return res;
